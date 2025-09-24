82f2a26dd1c5766220d8cb1c37aa02e1321378d8
Mark Luigi <markluigi@yahoo.it>
2025-09-24 13:45:38 +0200

    .

---
+++ src/app/components/color-block-component/color-block-component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-color-block-component',
+  imports: [],
+  templateUrl: './color-block-component.html',
+  styleUrl: './color-block-component.css'
+})
+export class ColorBlockComponent {
+
+}
